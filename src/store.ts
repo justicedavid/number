d9f8cd41a4f899ff7b8d6d353b1bfc1e36bba159
justicedavid <justicedavid999@yahoo.com>
2025-01-17 22:40:13 +0000

    Document build - handling edge cases

--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import AsyncStorage from '@react-native-community/async-storage'
-let persistConfig = {
+const persistConfig = {
     key: 'root',
     storage: AsyncStorage,
     whitelist: ['user']
 }
 import rootReducer from './reducers/'
 
-let persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 import thunk from 'redux-thunk';
-let middlewares = [thunk]
-let enhancers = [applyMiddleware(...middlewares)]
-export let store = createStore(persistedReducer, compose(...enhancers))
-export let persistor = persistStore(store)
+const middlewares = [thunk]
+const enhancers = [applyMiddleware(...middlewares)]
+export const store = createStore(persistedReducer, compose(...enhancers))
+export const persistor = persistStore(store)
