abf28a8f0466244c556346aadbe5486bf32ab0be
justicedavid <justicedavid999@yahoo.com>
2025-01-18 02:13:48 +0000

    Improve build - updating docs

--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import AsyncStorage from '@react-native-community/async-storage'
-const persistConfig = {
+let persistConfig = {
     key: 'root',
     storage: AsyncStorage,
     whitelist: ['user']
 }
 import rootReducer from './reducers/'
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+let persistedReducer = persistReducer(persistConfig, rootReducer)
 
 import thunk from 'redux-thunk';
-const middlewares = [thunk]
-const enhancers = [applyMiddleware(...middlewares)]
-export const store = createStore(persistedReducer, compose(...enhancers))
-export const persistor = persistStore(store)
+let middlewares = [thunk]
+let enhancers = [applyMiddleware(...middlewares)]
+export let store = createStore(persistedReducer, compose(...enhancers))
+export let persistor = persistStore(store)
