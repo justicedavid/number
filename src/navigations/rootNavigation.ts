bcb988c4a3cda6e7187f2ec79f60c92f6d0b28cc
justicedavid <justicedavid999@yahoo.com>
2025-02-08 19:57:00 +0000

    Optimize auth - improving performance

--- src/navigations/rootNavigation.ts
+++ src/navigations/rootNavigation.ts
@@ -4,9 +4,9 @@ import {
     NavigationContainerRef, NavigationAction
 } from '@react-navigation/native';
 
-export let navigationRef:
+export const navigationRef:
     React.RefObject<NavigationContainerRef> = React.createRef();
-// export let navigation = navigationRef.current
+// export const navigation = navigationRef.current
 export function navigate(name: string, params?: object): void {
     navigationRef.current?.navigate(name, params);
 }
@@ -22,7 +22,7 @@ export function push(name: string, params?: object): void {
 export function goBack(): void {
     navigationRef.current?.goBack();
 }
-export let navigation = {
+export const navigation = {
     navigate,
     dispatch,
     replace,
